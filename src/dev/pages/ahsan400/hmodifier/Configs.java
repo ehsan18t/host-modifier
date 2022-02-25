37fd7f2b4b596f0c788c68afcf28a049d38f7532
Ahsan Khan <nobody.ahsan@gmail.com>
2022-02-25 19:26:31 +0600

    initial commit

---
+++ src/dev/pages/ahsan400/hmodifier/Configs.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.pages.ahsan400.hmodifier;
+
+/**
+ *
+ * @author Ahsan
+ */
+public class Configs {
+    public static final String title = "Simple Host Modifier";
+    public static final String icon = "/res/icon.png";
+    public static final String version = "v1.0";
+}
---
+++ src/dev/pages/ahsan400/hmodifier/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.pages.ahsan400.hmodifier;
+
+/**
+ *
+ * @author Ahsan
+ */
+public class Main {
+    public static void main(String[] args) {
+        // start gui
+        java.awt.EventQueue.invokeLater(() -> new MainFrame().setVisible(true));
+        // System.out.println(Thread.currentThread().getId()); // debug
+    }
+}
